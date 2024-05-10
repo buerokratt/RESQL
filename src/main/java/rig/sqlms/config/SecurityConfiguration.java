c4cf6bc676a2c1ee3a23c29f5ed48a0fcf7473e3
Alar Floren <alar.floren@bpw-consulting.com>
2024-05-10 16:34:23 +0300

    Update Resql to Spring Boot 3

--- src/main/java/rig/sqlms/config/SecurityConfiguration.java
+++ src/main/java/rig/sqlms/config/SecurityConfiguration.java
@@ -4,19 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
     @Value("${headers.content-security-policy}")
     private String contentSecurityPolicy;
 
     public SecurityConfiguration() {
-        super(true);
+        super();
     }
 
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .headers()
